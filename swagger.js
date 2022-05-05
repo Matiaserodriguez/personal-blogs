5173cc3c962fb901984fa748355c9cbb9a831414
Matias <maturod96@gmail.com>
2022-05-04 21:44:12 -0300

    Fixes host

--- swagger.js
+++ swagger.js
@@ -5,15 +5,11 @@ const doc = {
     title: 'Blog Posts API',
     description: 'This is an API for Posting blogs with Authentication',
   },
-  host: 'nodejs-cse341.herokuapp.com',
+  host: 'cse341-personal-blog.herokuapp.com',
   schemes: ['https'],
 };
 
 const outputFile = 'swagger-output.json';
 const endpointsFiles = ['./src/routes/index.js'];
-
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
